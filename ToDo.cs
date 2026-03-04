648aedce955193c752af8c05e9a7dba6007cf838
Thane <thabay0606@students.maquoketaschools.org>
2026-03-04 14:20:34 -0600

    Made a while loop.

--- ToDo.cs
+++ ToDo.cs
@@ -2,4 +2,14 @@
 public class ToDo
 {
     // Add your code here
+    public string[] MarkAsDone(string[] todos){
+        int index = 0;
+
+        while(index < todos.Length){
+            todos[index] = "done - " + todos[index];
+            index++;
+        }
+
+            return todos;
+    }
 }
